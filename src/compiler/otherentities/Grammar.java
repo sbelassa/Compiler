af4957dddd8e1119ed4748fb9c1228a9fbdb550e
sbelassa <salma.belassal@gmail.com>
2017-02-19 22:47:23 +0100

    fixing bugs and adding javadoc

--- src/compiler/otherentities/Grammar.java
+++ src/compiler/otherentities/Grammar.java
@@ -2,8 +2,13 @@ package compiler.otherentities;
 
 import java.util.ArrayList;
 
+/**
+ * The grammar containing rules
+ * 
+ */
 public class Grammar {
 	
+	/// list of rules
 	private ArrayList<Node> rules;
 	
 	
@@ -42,7 +47,10 @@ public class Grammar {
 		return "Grammar [rules=" + rules + "]";
 	}
 	
-	
+	/*!
+	 * 
+	 * just a dumb function to print a character the number of times we give
+	 */
 	public void charPrinter(char c, int cnt){
 	    for(int i = 0; i < cnt; i++){
 	        System.out.print(c);
@@ -50,10 +58,13 @@ public class Grammar {
 	}
 	
 	
-
+	/**
+	 * printing a node using the dumb function to represent the levels of the tree nodes
+	 * recursive printing
+	 */
 	public void PrintNode(Node node,int cnt){
 		
-		charPrinter('-', cnt*2);
+		charPrinter('-', cnt*2);/// two '-' representing one level
 		System.out.println(node.toString());		
 		
 		if(node.getLeft()!=null){
@@ -61,7 +72,6 @@ public class Grammar {
 			
 		}
 		//System.out.println("\n");
-
 		if(node.getRight()!=null){
 			
 			PrintNode(node.getRight(), cnt+1);
@@ -70,22 +80,20 @@ public class Grammar {
 		
 	}
 
-
+	///printing the tree that contains the nodes just by calling the function that prints nodes
 	public void PrintTree(int ruleNum){
+		
 		//if(rules.get(ruleNum))
-		
 		PrintNode(rules.get(ruleNum), 1);
-		
-		
 	}
 	
 	
 	public static void main(String[] args) {
 
-		//the grammar containing all 5 rules
+		///the grammar containing all 5 rules
 		Grammar G= new Grammar();
 		
-		//first rule
+		///first rule
 		Node rule1= new Conc(
 							new Star(
 								new Conc(
@@ -97,10 +105,10 @@ public class Grammar {
 										new Atom(",", AtomType.Terminal,1))), 
 							new Atom(";", AtomType.Terminal,0));
 		
-		//second rule
+		///second rule
 		Node rule2= new Atom("IDNTER",AtomType.Terminal,0);
 		
-		//third rule
+		///third rule
 		Node rule3= new Conc(
 							new Star(
 									new Conc(
@@ -108,7 +116,7 @@ public class Grammar {
 											new Atom("T",AtomType.NonTerminal,0))),
 							new Atom("T",AtomType.NonTerminal,0));				
 		
-		//fourth rule
+		///fourth rule
 		Node rule4= new Conc(
 							new Star(
 									new Conc(
@@ -116,20 +124,20 @@ public class Grammar {
 											new Atom("F", AtomType.NonTerminal,0))), 
 							new Atom("F", AtomType.NonTerminal,0));
 		
-		//fifth rule
-		//Node rule5= new Node();
+		///fifth rule
+		///Node rule5= new Node();
 
 		
-		//adding rules to the grammar
+		///adding rules to the grammar
 		G.addRule(rule1);
 		G.addRule(rule2);
 		G.addRule(rule3);
 		G.addRule(rule4);
-		//G.addRule(rule5);
+		///G.addRule(rule5);
 
-		//System.out.println(node.toString());
+		///System.out.println(node.toString());
 		
-		//printing all 5 rules
+		///printing all 5 rules
 		for(int i=0;i<G.rules.size();i++){
 			System.out.println("\n****** Printing Rule number "+i+" :\n ");
 			G.PrintTree(i);
