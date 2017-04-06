3400688c98a2332db680f155248e0a5d97f6fc3f
loyden <loyden@loyden-N60Dp>
2017-04-06 03:10:45 +0200

    go action

---
+++ src/compiler/entities/Stack.java
@@ -0,0 +1,40 @@
+package compiler.entities;
+
+public class Stack {
+
+	
+	final static int SIZE = 20; // assez grand ?
+
+	private int sp ;
+	private Node [] t ;
+
+	
+	/**
+	 * Constructor
+	 */
+	public Stack () { // Construire une pile vide
+	    t = new Node[SIZE] ; sp = 0;
+	}
+	
+	
+	
+	/**
+	 * Test is the Stack is empty
+	 * @return
+	 */
+	public boolean isEmpty() { return sp == 0 ; }
+
+	
+	//
+	public void push(Node x) {
+		if (sp >= SIZE) throw new Error ("Push : pile pleine") ;
+	    t[sp++] = x ; 
+	}
+
+	
+	
+	public Node pop() {
+	    if (isEmpty()) throw new Error ("Pop : pile vide") ;
+	    return t[--sp] ; 
+	}
+}
